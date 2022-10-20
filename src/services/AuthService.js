88b6a19de30f59fcaec18087886170424897bbf6
supachok jrirarojkul <supachok8728@gmail.com>
2022-10-20 23:45:09 +0700

    7

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -40,5 +40,12 @@ export default {
     } else {
       return false
     }
+  },
+  register(user) {
+    return apiClient.post('/register', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    })
   }
 }
